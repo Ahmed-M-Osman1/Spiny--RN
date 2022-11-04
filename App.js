5d607f9f0cff5e929d33447e1849ddbbca632fc2
akiid777 <akiid777@gmail.com>
2022-11-04 22:19:55 +0200

    Add splash screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from './src/screens/splashScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Open up App.js to start working on your app!</Text>
+      <SplashScreen />
     </View>
   );
 }
